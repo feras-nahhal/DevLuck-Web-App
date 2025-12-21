b9cc8aac38d952d89878de2fac8800eb76aa19fe
Feras Hani <122812085+feras-nahhal@users.noreply.github.com>
2025-12-22 00:04:36 +0200

    Devluck v1.9

---
+++ src/mocks/mockCompanies.ts
@@ -0,0 +1,315 @@
+export const mockCompanies = [
+  {
+    id: "C001",
+    name: "DevLuck Technologies",
+    address: "123 Innovation Street",
+    city: "Boston",
+    email: "contact@devluck.com",
+    phoneNumber: "+1 617 555 0101",
+    employeeNumber: 320,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E101", "E102", "E103", "E104", "E105"],
+    profileRanking: 9,
+    progress: 85,
+    status: "Verified",
+    programs: ["Internship", "Graduate Program", "Full-time"],
+    image: "/images/C12.jpeg",
+  },
+  {
+    id: "C002",
+    name: "NextWave Solutions",
+    address: "45 Harbor Avenue",
+    city: "San Francisco",
+    email: "hello@nextwave.io",
+    phoneNumber: "+1 415 555 0198",
+    employeeNumber: 210,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E201", "E202", "E203"],
+    profileRanking: 8,
+    progress: 72,
+    status: "Pending",
+    programs: ["Internship", "Remote Jobs"],
+    image: "/images/C12.jpeg",
+  },
+  {
+    id: "C003",
+    name: "BrightCore Systems",
+    address: "78 Tech Park Road",
+    city: "Austin",
+    email: "info@brightcore.com",
+    phoneNumber: "+1 512 555 0147",
+    employeeNumber: 150,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E301", "E302"],
+    profileRanking: 7,
+    progress: 60,
+    status: "Verified",
+    programs: ["Junior Roles", "Full-time"],
+    image: "/images/C12.jpeg",
+  },
+  {
+    id: "C004",
+    name: "CloudNova Inc.",
+    address: "900 Skyway Blvd",
+    city: "Seattle",
+    email: "support@cloudnova.com",
+    phoneNumber: "+1 206 555 0184",
+    employeeNumber: 480,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E401", "E402", "E403", "E404"],
+    profileRanking: 10,
+    progress: 95,
+    status: "Verified",
+    programs: ["Internship", "Graduate Program", "Leadership Track"],
+    image: "/images/C11.jpeg",
+  },
+  {
+    id: "C005",
+    name: "AlphaBridge Group",
+    address: "12 Market Square",
+    city: "New York",
+    email: "contact@alphabridge.com",
+    phoneNumber: "+1 212 555 0172",
+    employeeNumber: 90,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E501"],
+    profileRanking: 6,
+    progress: 40,
+    status: "Pending",
+    programs: ["Contract", "Part-time"],
+    image: "/images/C11.jpeg",
+  },
+  {
+    id: "C006",
+    name: "QuantumSoft Labs",
+    address: "77 Massachusetts Avenue Cambridge US",
+    city: "Chicago",
+    email: "team@quantumsoft.ai",
+    phoneNumber: "+1 312 555 0139",
+    employeeNumber: 260,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E601", "E602", "E603"],
+    profileRanking: 80,
+    progress: 78,
+    status: "Verified",
+    programs: [
+  "Internship",
+  "AI Fellowship",
+  "Graduate Program",
+  "Remote Jobs",
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Leadership Track",
+  "Junior Roles",
+  "Medical Internship",
+  "IT Healthcare Program",
+  "Engineering Program",
+  "Finance Graduate Program",
+  "Analyst Roles",
+  "UI/UX Internship",
+  "Marketing Roles"
+],
+
+    image: "/images/C11.jpeg",
+  },
+  {
+    id: "C007",
+    name: "GreenAxis Energy",
+    address: "5 Renewable Way",
+    city: "Denver",
+    email: "info@greenaxis.com",
+    phoneNumber: "+1 303 555 0166",
+    employeeNumber: 180,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E701", "E702"],
+    profileRanking: 7,
+    progress: 68,
+    status: "Pending",
+    programs: ["Engineering Program", "Graduate Program"],
+    image: "/images/C11.jpeg",
+  },
+  {
+    id: "C008",
+    name: "BlueOrbit Media",
+    address: "210 Creative Lane",
+    city: "Los Angeles",
+    email: "hello@blueorbit.media",
+    phoneNumber: "+1 323 555 0192",
+    employeeNumber: 120,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E801", "E802", "E803"],
+    profileRanking: 6,
+    progress: 55,
+    status: "Verified",
+    programs: ["Content Internship", "Marketing Roles"],
+    image: "/images/C11.jpeg",
+  },
+  {
+    id: "C009",
+    name: "IronPeak Finance",
+    address: "88 Wall Street",
+    city: "New York",
+    email: "careers@ironpeakfinance.com",
+    phoneNumber: "+1 212 555 0119",
+    employeeNumber: 540,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E901", "E902", "E903", "E904"],
+    profileRanking: 9,
+    progress: 90,
+    status: "Verified",
+    programs: ["Finance Graduate Program", "Analyst Roles"],
+    image: "/images/C11.jpeg",
+  },
+  {
+    id: "C010",
+    name: "NovaHealth Systems",
+    address: "300 Wellness Drive",
+    city: "San Diego",
+    email: "hr@novahealth.org",
+    phoneNumber: "+1 619 555 0180",
+    employeeNumber: 410,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E1001", "E1002"],
+    profileRanking: 8,
+    progress: 82,
+    status: "Pending",
+    programs: ["Medical Internship", "IT Healthcare Program"],
+    image: "/images/C11.jpeg",
+  },
+    {
+    id: "C011",
+    name: "PixelForge Studios",
+    address: "17 Design District",
+    city: "Portland",
+    email: "contact@pixelforge.studio",
+    phoneNumber: "+1 503 555 0142",
+    employeeNumber: 75,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E1101", "E1102"],
+    profileRanking: 6,
+    progress: 48,
+    status: "Pending",
+    programs: ["UI/UX Internship", "Frontend Roles"],
+    image: "/images/C12.jpeg",
+  },
+  {
+    id: "C012",
+    name: "AeroLink Dynamics",
+    address: "420 Aviation Blvd",
+    city: "Dallas",
+    email: "info@aerolinkdynamics.com",
+    phoneNumber: "+1 972 555 0175",
+    employeeNumber: 360,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E1201", "E1202", "E1203", "E1204"],
+    profileRanking: 9,
+    progress: 88,
+    status: "Verified",
+    programs: ["Aerospace Internship", "Mechanical Graduate Program"],
+    image: "/images/C11.jpeg",
+  },
+  {
+    id: "C013",
+    name: "DataSpring Analytics",
+    address: "58 Insight Street",
+    city: "Atlanta",
+    email: "careers@dataspring.ai",
+    phoneNumber: "+1 404 555 0169",
+    employeeNumber: 140,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E1301", "E1302", "E1303"],
+    profileRanking: 7,
+    progress: 65,
+    status: "Verified",
+    programs: ["Data Internship", "Analytics Graduate Track"],
+    image: "/images/C12.jpeg",
+  },
+  {
+    id: "C014",
+    name: "UrbanNest Developments",
+    address: "990 Central Plaza",
+    city: "Miami",
+    email: "hello@urbannest.dev",
+    phoneNumber: "+1 305 555 0128",
+    employeeNumber: 220,
+    corporate: `“Mind and Hand” is the thought-provoking motto of the Massachusetts Institute of Technology, known also as MIT. 
+This motto encapsulates this famous institution’s mission to advance knowledge in science, technology, and areas 
+of scholarship that can help make the world a better place. At its founding in 1861, MIT was initially a small 
+community of problem-solvers and science lovers eager to bring their knowledge to bear on the world. Today, MIT 
+has evolved into an educational behemoth, with some 1,000 faculty members and more than 11,000 undergraduate 
+and graduate students.`,
+    employeeIds: ["E1401", "E1402"],
+    profileRanking: 7,
+    progress: 70,
+    status: "Pending",
+    programs: ["Architecture Internship", "Project Management Roles"],
+    image: "/images/C11.jpeg",
+  },
+
+];
